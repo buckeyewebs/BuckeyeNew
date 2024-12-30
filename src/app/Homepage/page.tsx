7245336c65df5b0159ae530601f7c10deb5d8ce6
Fatima270 <fatimaabbasi270@gmail.com>
2024-12-30 22:59:18 +0500

    Commit

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -14,14 +14,14 @@ export default function HomePage(){
           <Header />
           {/* Main Content */}
           <div className="w-[1280px] h-auto">
-            <div className="w-[1280px] h-auto pt-[50px] pr-[108px] pl-[81px] pb-[20px] sm:pb-[130px] md:pb-[140px] lg:pb-[152px] flex flex-row md:flex-col">
-              <div className="mt-[20px] mb-[37px] w-[549px] h-auto flex items-center justify-center md:items-start md:justify-start">
+            <div className="w-[1280px] h-auto pt-[50px] pr-[108px] pl-[81px] pb-[20px] sm:pb-[130px] md:pb-[140px] lg:pb-[152px] flex flex-row">
+              <div className="mt-[20px] mb-[37px] w-[549px] h-auto flex items-center">
                 <p className="font-lancelot lg:text-[68px] tracking-tight leading-[1.3] text-[40px] sm:text-[46px] md:text-[58px]  ">
                   Building Websites <br />
                   that grow your <br />
                   business
                 </p>
-                <div  className="w-[275px] h-[60px] mt-[20px] rounded-[43px]  border border-black pt-[17px] pl-[53px] transition duration-300 ease-in-out hover:bg-black hover:text-white flex items-center justify-center md:items-start md:justify-start">
+                <div  className="w-[275px] h-[60px] mt-[20px] rounded-[43px]  border border-black pt-[17px] pl-[53px] transition duration-300 ease-in-out hover:bg-black hover:text-white">
                 <Link href="/QuoteRequestForm">
                   <button  className="mb-[37px] ">
                     REQUEST A QUOTE
