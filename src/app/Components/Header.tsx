bfc0dc9b7a5a7c96f7c62f3354a3ca35bdb5cfe4
Fatima270 <fatimaabbasi270@gmail.com>
2024-12-31 00:21:33 +0500

    Commit

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
           height={71}
           style={{ marginLeft: "0.92%", paddingTop: "3%" }}
         />
-        <div className="w-[238px] h-[50px] mt-[23px] mb-[0.85%] ml-[3.125%]">
+        <div className="w-[238px] h-[50px] mt-[23px] mb-[0.85%] ml-[1.125%]">
           <p className="font-inknut text-clamp(16px, 4vw, 32px)">BUCKEYE</p>
           <p className="font-inknut text-clamp(12px, 1.37vw, 20px) ml-[3.33%]">-WEB SOLUTIONS-</p>
         </div>
