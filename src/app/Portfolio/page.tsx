5e77085cb35afc030d62c9d16f7913d34d94c052
Fatima270 <fatimaabbasi270@gmail.com>
2024-12-31 22:02:13 +0500

    Commit New

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -54,9 +54,9 @@ export default function Portfolio() {
                     <div className="flex flex-col lg:flex-row lg:mt-[100px]">
                         <div>
                             <p className="font-istokWeb lg:text-[18px] ml-[20px]">Featured Project</p>
-                            <p className="font-istok lg:text-[30px]  ml-[20px]">Example Project</p>
-                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio video comparison and tech details.
-                            </p>
+                            <p className="font-istok lg:text-[30px]  ml-[20px]">AutoMarket Pro</p>
+                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">
+                            A Java-based platform enabling seamless car buying and selling with dedicated consoles for sellers, buyers, and admins. It ensures efficient listings, secure transactions, and streamlined management.</p>
 
                         </div>
                         <div className="sm:w-[400px] md:w-[450px] lg:w-[523px] w-[340px]  h-auto bg-black ml-[100px] md:mt-[20px] sm:mt-[20px] lg:mt-[0px] mt-[20px]">
@@ -76,8 +76,8 @@ export default function Portfolio() {
                         </div>
                         <div>
                             <p className="font-istokWeb lg:text-[18px] ml-[20px] mt-[20px]">Featured Project</p>
-                            <p className="font-istok lg:text-[30px]  ml-[20px]">Example Project</p>
-                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio video comparison and tech details.
+                            <p className="font-istok lg:text-[30px]  ml-[20px]">Turbo Trader</p>
+                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A MERN stack-based car-sharing platform, enabling users to rent vehicles from local owners. It features seamless booking, secure transactions, and efficient management.
                             </p>
                         </div>
 
@@ -90,8 +90,8 @@ export default function Portfolio() {
                     <div className="flex flex-col lg:mt-[100px] md:mt-[80px] sm:mt-[60px] mt-[45px]">
                     <div>
                             <p className="font-istokWeb lg:text-[18px] ml-[20px]">Featured Project</p>
-                            <p className="font-istok lg:text-[30px]  ml-[20px]">Example Project</p>
-                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio video comparison and tech details.
+                            <p className="font-istok lg:text-[30px]  ml-[20px]">Turbo Trader</p>
+                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A MERN stack-based car-sharing platform, enabling users to rent vehicles from local owners. It features seamless booking, secure transactions, and efficient management.
                             </p>
 
                         </div>
@@ -109,8 +109,8 @@ export default function Portfolio() {
                     <div className="flex flex-col lg:flex-row lg:mt-[100px] md:mt-[80px] sm:mt-[60px] mt-[45px] mb-[20px]">
                         <div>
                             <p className="font-istokWeb lg:text-[18px] ml-[20px]">Featured Project</p>
-                            <p className="font-istok lg:text-[30px]  ml-[20px]">Example Project</p>
-                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio video comparison and tech details.
+                            <p className="font-istok lg:text-[30px]  ml-[20px]">Gemini Pod</p>
+                            <p className="lg:w-[600px] ml-[20px] lg:p-[15px] lg:h-[148.2px] font-istokWeb text-[18px] bg-custom-radial flex justify-center items-center  rounded-[14px]">A Google Gemini AI pod in Flutter that effortlessly tackles both image and text queries. 🚀 Leveraging robust packages for a seamless fusion of creativity and functionality.
                             </p>
 
                         </div>
