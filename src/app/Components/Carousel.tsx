ff75ef88cc51bc3bfc38f69b1674b8fc81e0a906
Fatima270 <fatimaabbasi270@gmail.com>
2024-12-31 20:17:02 +0500

    Commit New

--- src/app/Components/Carousel.tsx
+++ src/app/Components/Carousel.tsx
@@ -4,7 +4,6 @@ import Slider from 'react-slick';
 import { useRef } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image from 'next/image';
 
 const Carousel = () => {
   const sliderRef = useRef<Slider | null>(null);
