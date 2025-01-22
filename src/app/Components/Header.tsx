bb7331da12c44fdcbdf46ce159392e1f71125f8e
Fatima270 <fatimaabbasi270@gmail.com>
2025-01-23 01:58:08 +0500

    Commit after changes in size of text

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -14,36 +14,36 @@ export default function Header() {
     <div className="w-full h-auto bg-[rgba(112,154,90,1)]">
       <div className="w-full h-auto   flex flex-row flex justify-between ">
         <div className='flex flex-row items-center w-full h-auto   '>
-          <Image
+          <img
             alt='logo'
             src="/assets/svgs/logo.svg"
             // width={108}
             // height={71}
             // className={{ marginLeft: "17.77px", paddingTop: "12px" }}
-            className='ml-[2%]  w-[17%] md:w-[15%] lg:w-[10%] xl:w-[12%] 2xl:w-[10%] 3xl:w-[13%] 4xl:w-[12%] 5xl:w-[12%] h-auto pt-[1%]'
+            className='ml-[2%]  w-[20%] md:w-[10%] lg:w-[10%] xl:w-[10%] 2xl:w-[7.5%] 3xl:w-[7%] 4xl:w-[7%] 5xl:w-[7%] h-auto pt-[1%]'
           />
-          <div className="w-auto   h-auto  mt-[2%]   mb-[1%]  pb-[1%] flex flex-col justify-center items-center">
-            <p className="font-inknut pt-[2%]   text-[18px] md:text-[12px] lg:text-[15px] xl:text-[20px]  2xl:text-[20px] 3xl:text-[35px] 4xl:text-[39px] 5xl:text-[60px]">BUCKEYE</p>
-            <p className="font-inknut   text-[15px] md:text-[20px] lg:text-[16px] xl:text-[`2px] 2xl:text-[20px] 3xl:text-[25px] 4xl:text-[24px] 5xl:text-[49px] whitespace-nowrap">-WEB SOLUTIONS-</p>
+          <div className="w-auto   h-auto  mt-[2%]   pb-[1%]   flex flex-col justify-center items-center">
+            <p className="font-inknut pt-[2%]   text-[18px] md:text-[12px] lg:text-[15px] xl:text-[20px]  2xl:text-[14px] 3xl:text-[25px] 4xl:text-[29px] 5xl:text-[30px]">BUCKEYE</p>
+            <p className="font-inknut   text-[15px] md:text-[20px] lg:text-[16px] xl:text-[`2px] 2xl:text-[12px] 3xl:text-[15px] 4xl:text-[17px] 5xl:text-[29px] whitespace-nowrap">-WEB SOLUTIONS-</p>
           </div>
         </div>
 
         {/* Desktop Menu (visible only on large screens) */}
         <div className='flex justify-center items-center mr-[10%]'>
-        <div className="whitespace-nowrap w-auto   flex items-center gap-[5%]  mb-[2%] hidden lg:flex">
-          <a href="/Homepage" className="link-underline  font-istok  text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[24px] 3xl:text-[25px] 4xl:text-[35px] 5xl:text-[49px] text-white">
+        <div className="whitespace-nowrap w-auto   flex items-center gap-[5%]  hidden lg:flex">
+          <a href="/Homepage" className="link-underline  font-istok  text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[14px] 3xl:text-[20px] 4xl:text-[25px] 5xl:text-[39px] text-white">
             Home
           </a>
-          <a href="/Services" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[24px] 3xl:text-[25px] 4xl:text-[35px] 5xl:text-[49px] text-white">
+          <a href="/Services" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[14px] 3xl:text-[20px] 4xl:text-[25px] 5xl:text-[39px] text-white">
             Services
           </a>
-          <a href="/Portfolio" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[24px] 3xl:text-[25px] 4xl:text-[35px] 5xl:text-[49px] text-white">
+          <a href="/Portfolio" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[14px] 3xl:text-[20px] 4xl:text-[25px] 5xl:text-[39px] text-white">
             Portfolio
           </a>
-          <a href="/About_Us" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[24px] 3xl:text-[25px] 4xl:text-[35px] 5xl:text-[49px] text-white">
+          <a href="/About_Us" className="link-underline  font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[14px] 3xl:text-[20px] 4xl:text-[25px] 5xl:text-[39px] text-white">
             About us
           </a>
-          <a href="/ContactUs" className="link-underline font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[24px] 3xl:text-[25px] 4xl:text-[35px] 5xl:text-[49px] text-white">
+          <a href="/ContactUs" className="link-underline font-istok text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[14px] 3xl:text-[20px] 4xl:text-[25px] 5xl:text-[39px] text-white">
             Contact us
           </a>
         </div>
